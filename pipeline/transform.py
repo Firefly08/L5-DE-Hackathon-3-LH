30aa3aec4349052e5241a7e03427e5458f9846cd
Firefly8 <horearem@gmail.com>
2025-07-30 09:05:08 +0000

    Changes1 Hackathon

---
+++ pipeline/transform.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import datetime
+from pipeline import utils
+
+def clean_daytime_sheets(sheets, column_names):
+    """ Processing all sheets and concatenating them into a single DataFrame """
+    all_sheets = []
+    for k in sheets.keys():
+        month, year = get_month_year(k)   
+        df = sheets[k]
+        
+        # change column names
+        df.columns = column_names
+        # or
+        # df = df.drop(df.index[[0, 1]])
+        
+        # drop first n rows and any empties
+        df = drop_rows(df, 2)
+        df = drop_empty_rows(df).reset_index(drop=True)
+        
+        # create sheet flag
+        # df['sheet'] = k 
+        
+        # clean columns
+        for col in df.columns:
+            if col != 'date':
+                df[col] = df[col].apply(clean_columns)
+        for col in ['sunrise','sunset','solar_noon_time']:
+            df[col] = df[col].str.split('(', expand=True).loc[:,0] 
+            
+        # update date
+        df["date"] = df["date"].apply(lambda x: datetime.date(year, month, int(x)))
+        all_sheets.append(df)
+        
+    return pd.concat(all_sheets, ignore_index=True)
+
+
+def clean_weather_sheets(sheets, column_names):
+    """ Processing all sheets and concatenating them into a single DataFrame """
+    all_sheets = []
+    for k in sheets.keys():
+        # print(f"YYMM: {k}")
+        month, year = get_month_year(k)
+        df = sheets[k]
+        
+        # # change column names
+        df.columns = column_names
+        
+        # # drop first five rows and any empties
+        df = drop_rows(df, 5)
+        df = drop_empty_rows(df).reset_index(drop=True)
+        # # or
+        # # df = df.drop(df.index[[0, 1]])
+        
+        # define snow column
+        df = define_snow(df)
+        
+        # # clean columns
+        for col in df.columns:
+            if col not in ['date', 'wind_direction']:
+                df[col] = df[col].apply(clean_columns)
+        # update date
+        df = define_date(df, year, month)
+        all_sheets.append(df)
+        
+    return pd.concat(all_sheets, ignore_index=True)
---
+++ pipeline/utils.py
@@ -0,0 +1,49 @@
+import re
+import numpy as np
+
+def drop_empty_rows(df, thresh=0.5):
+    """
+    Drops a row if at least "thresh%" is missing.
+    """
+    return df.dropna(thresh=int(thresh*len(df.columns))).reset_index(drop=True)
+
+def clean_columns(x):
+    """ Clean columns by replacing non-ascii and 'bad' characters """
+    x = str(x).replace('−', '-')
+    x = re.sub(r"[^().+\-:\d]", '', x)
+    return x
+
+def get_month_year(yymm, century=20):
+    """ Get month and year from YYMM string """
+    # print(f"YYMM: {k}")
+    month = int(yymm[2:])
+    year = century*100 + int(yymm[:2])
+    return month, year
+
+def load_sheets(file_path):
+    """ Load Excel workbook with all sheets"""
+    return pd.read_excel(file_path, sheet_name=None)
+
+def drop_rows(df, n):
+    """ Drop the leading n rows from a DataFrame """
+    # drop first n rows and any empties
+    return df.drop(index=range(n)).reset_index(drop=True)
+
+# define snow column
+def define_snow(df):
+    """ Define snow column based on rain_mm """
+    df['snow'] = np.where(df['rain_mm'].astype(str).str.endswith('s'),
+                        df['rain_mm'], 
+                        None)
+    df['rain_mm'] = np.where(df['rain_mm'].astype(str).str.endswith('s'),
+                        None, 
+                        df['rain_mm'])
+    return df
+
+def define_date(df, year, month):
+    df["date"] = df["date"].apply(lambda x: datetime.date(year, month, int(x)))
+    return df
+
+def merge_datasets(df1, df2):
+    """ Merge two DataFrames on 'date' column """
+    return df1.merge(df2, on='date', how='inner')
